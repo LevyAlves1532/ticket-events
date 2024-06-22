1fd07085042c3d7e89ca03f7e85e3c931c085943
Lêvy <levy.pereiraA1532@gmail.com>
2024-06-22 06:10:09 -0300

    Ajustando rotas

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,11 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/events', [EventController::class, 'index'])->name('events');
-Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
-Route::post('/events/create_action', [EventController::class, 'create_action'])->name('events.create_action');
-Route::get('/events/update/{id}', [EventController::class, 'update'])->name('events.update');
-Route::post('/events/update_action', [EventController::class, 'update_action'])->name('events.update_action');
-Route::get('/events/delete/{id}', [EventController::class, 'delete'])->name('events.delete');
+Route::prefix('events')->group(function() {
+    Route::get('/', [EventController::class, 'index'])->name('events');
+    Route::get('/create', [EventController::class, 'create'])->name('events.create');
+    Route::post('/create_action', [EventController::class, 'create_action'])->name('events.create_action');
+    Route::get('/update/{id}', [EventController::class, 'update'])->name('events.update');
+    Route::post('/update_action', [EventController::class, 'update_action'])->name('events.update_action');
+    Route::get('/delete/{id}', [EventController::class, 'delete'])->name('events.delete');
+});
